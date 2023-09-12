846638e3ee399e763443376dd2eb50ea5fe11b5b
misswallstreet <latisha@latishastyles.com>
2023-09-12 15:41:26 -0400

    add icon

--- api/index.py
+++ api/index.py
@@ -53,7 +53,7 @@ def sanitize_channel_name(name):
 
 @app.route('/')
 def index():
-    return render_template('/api/index.html')
+    return render_template('index.html')
 
 
 @app.route('/get_channel_id', methods=['POST'])
@@ -133,7 +133,7 @@ def get_json_data():
     else:
         return jsonify({'status': 'error', 'message': 'Submissions are still open.'})
 
-#
-# if __name__ == '__main__':
-#     port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable provided by Vercel; default to 5000 for local development
-#     app.run(host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable provided by Vercel; default to 5000 for local development
+    app.run(host='0.0.0.0', port=port)
